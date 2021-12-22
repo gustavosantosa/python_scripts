27f8c29d29fdbbfbbee6b8a76362965dbbd7c38c
gustavosantosa <96523690+gustavosantosa@users.noreply.github.com>
2021-12-22 10:59:49 +0100

    Update multiple_lines_no_errors.py

--- multiple_lines_no_errors.py
+++ multiple_lines_no_errors.py
@@ -8,15 +8,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import ntpath
 
-filename = ('08072021_Gustavo_hahella.csv')
+filename = ('.csv')
 data = pd.read_csv(filename, sep=",", decimal=".")
 
 print (ntpath.splitext(filename)[0])
 print (data.head())
 
 data.plot(0,[1,2,3],linestyle="--",linewidth=0.5 , marker="o" ,markersize=4)
-plt.xlabel('Time [min]')
-plt.ylabel('ABS [473nm]')
+plt.xlabel('xlabel')
+plt.ylabel('ylabel')
 plt.title(ntpath.splitext(filename)[0])
 
 plt.savefig(ntpath.splitext(filename)[0]+' no errors',dpi=300)#
